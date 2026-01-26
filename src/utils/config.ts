33804c0902f9b930d969e3aa34afa389c85f0788
こぽー <tora1025oishi@gmail.com>
2026-01-27 05:32:03 +0900

    環境変数の設定

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
-} as const; // 読み取り専用
+  SUPABASE_URL: process.env.SUPABASE_URL ?? '',
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY ?? '',
+} as const;
