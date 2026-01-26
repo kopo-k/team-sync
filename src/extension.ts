e33f4754f07f88ff3902260a0f42ede5459ee507
こぽー <tora1025oishi@gmail.com>
2026-01-26 16:11:21 +0900

    エントリーポイント

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { loginCommand, logoutCommand } from './commands/auth';
+import { createTeamCommand, joinTeamCommand } from './commands/team';
+import { setStatusCommand } from './commands/status';
+import { TeamSyncSidebarProvider } from './views/sidebarProvider';
+import { startFileWatcher } from './watchers/fileWatcher';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('TeamSync is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "team-sync" is now active!');
+  // サイドバー
+  const sidebarProvider = new TeamSyncSidebarProvider();
+  vscode.window.registerTreeDataProvider('teamSyncSidebar', sidebarProvider);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('team-sync.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from team-sync!');
-	});
+  // コマンド登録
+  context.subscriptions.push(
+    vscode.commands.registerCommand('team-sync.login', loginCommand),
+    vscode.commands.registerCommand('team-sync.logout', logoutCommand),
+    vscode.commands.registerCommand('team-sync.createTeam', createTeamCommand),
+    vscode.commands.registerCommand('team-sync.joinTeam', joinTeamCommand),
+    vscode.commands.registerCommand('team-sync.setStatus', () => {
+      // TODO: 現在のメンバーIDを渡す
+      setStatusCommand('');
+    })
+  );
 
-	context.subscriptions.push(disposable);
+  // ファイル監視開始
+  startFileWatcher(context);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
